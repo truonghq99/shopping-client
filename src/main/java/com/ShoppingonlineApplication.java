33c56799010b318bb917012a944dc92adeac3146
truonghq99 <truonghq.99@gmail.com>
2021-07-20 20:39:49 +0700

    a

--- src/main/java/com/ShoppingonlineApplication.java
+++ src/main/java/com/ShoppingonlineApplication.java
@@ -1,7 +1,12 @@
 package com;
 
+import com.model.Staff;
+import com.repository.StaffRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class ShoppingonlineApplication {
@@ -9,5 +14,21 @@ public class ShoppingonlineApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ShoppingonlineApplication.class, args);
 	}
+
+	// @Autowired
+    // StaffRepository repo;
+    // @Autowired
+    // PasswordEncoder passwordEncoder;
+
+    // @Override
+    // public void run(String[] args) throws Exception {
+    //     // Khi chương trình chạy
+    //     // Insert vào csdl một user.
+    //     Staff staff = new Staff();
+    //     staff.setUsername("administrator");
+    //     staff.setPassword(passwordEncoder.encode("1234546"));
+    //     repo.save(staff);
+    //     System.out.println(staff);
+    // }
 
 }
--- src/main/java/com/WebSecurityConfig.java
+++ src/main/java/com/WebSecurityConfig.java
@@ -33,7 +33,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/login").permitAll() // Cho phép tất cả mọi người truy cập vào 2 địa chỉ này
+                .antMatchers("/signup","/", "/login").permitAll() // Cho phép tất cả mọi người truy cập vào 2 địa chỉ này
                 .anyRequest().authenticated() // Tất cả các request khác đều cần phải xác thực mới được truy cập
                 .and()
                 .formLogin() // Cho phép người dùng xác thực bằng form login
